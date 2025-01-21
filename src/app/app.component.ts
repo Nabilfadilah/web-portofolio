48d9aac9fcf2ffb7dca209e7a5d4abb5f6eb64fc
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-21 19:40:26 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButtonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppComponent {
   title = 'web-portofolio';
