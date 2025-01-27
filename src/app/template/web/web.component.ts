472f6bb7b5c335432a640a57ac4d18936e56db21
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-27 16:11:41 +0700

    Mengimport component about us untuk di tampilkan

--- src/app/template/web/web.component.ts
+++ src/app/template/web/web.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '@components/header/header.component';
 import { BannerComponent } from '@components/banner/banner.component';
 import { HeaderMenuComponent } from "@components/header-menu/header-menu.component";
+import { AboutUsComponent } from "../../components/about-us/about-us.component";
 
 @Component({
   selector: 'app-web',
-  imports: [HeaderComponent, BannerComponent, HeaderMenuComponent],
+  imports: [HeaderComponent, BannerComponent, HeaderMenuComponent, AboutUsComponent],
   templateUrl: './web.component.html',
   styleUrl: './web.component.css'
 })
