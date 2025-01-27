05e16f18b99b2114918afc9edb7bb08eba1cf38f
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-27 17:20:25 +0700

    Import compoenent yang dibuthkan dan membuat array list data dan on pagination

--- src/app/components/news/news.component.ts
+++ src/app/components/news/news.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
-
+import { Component, CUSTOM_ELEMENTS_SCHEMA, ElementRef, ViewChild } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { SwiperContainer } from 'swiper/element';
 @Component({
   selector: 'app-news',
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './news.component.html',
-  styleUrl: './news.component.css'
+  styleUrl: './news.component.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class NewsComponent {
+  @ViewChild('newSwiper') swiper!: ElementRef<SwiperContainer>;
 
+  listNews = [
+    {
+      title: 'Contoh 1',
+      tag: 'berita 1',
+      datePost: new Date('2022-10-12'),
+      author: 'ALIF',
+      image: 'images/bpjs.png'
+    },
+    {
+      title: 'Contoh 2',
+      tag: 'berita 2',
+      datePost: new Date('2022-10-12'),
+      author: 'ALIF',
+      image: 'images/kis.jpg'
+    },
+    {
+      title: 'Contoh 3',
+      tag: 'berita 3',
+      datePost: new Date('2022-10-12'),
+      author: 'ALIF',
+      image: 'images/ketenaga.jpg'
+    },
+  ]
+
+  onPrev() {
+    this.swiper.nativeElement.swiper.slideNext();
+  }
+  onNext() {
+    this.swiper.nativeElement.swiper.slideNext();
+  }
 }
