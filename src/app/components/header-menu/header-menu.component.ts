14d1d020b005f1b7cba70923c12e618c3a32463f
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-27 11:27:12 +0700

    Import module MatMenu, MatList, MatToolbar dan MatIcon

--- src/app/components/header-menu/header-menu.component.ts
+++ src/app/components/header-menu/header-menu.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { MatToolbar } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header-menu',
-  imports: [],
+  imports: [MatToolbar, MatMenuModule, MatListModule, MatIconModule],
   templateUrl: './header-menu.component.html',
   styleUrl: './header-menu.component.css'
 })
