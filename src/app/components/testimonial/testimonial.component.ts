268c5f200a943bdec9234f658fab8767da114879
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-27 22:12:57 +0700

    Import component yang dibutihkan dan membuat testimonial list data

--- src/app/components/testimonial/testimonial.component.ts
+++ src/app/components/testimonial/testimonial.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
-
+import { Component, CUSTOM_ELEMENTS_SCHEMA, ViewChild, ElementRef } from '@angular/core';
+import { SwiperContainer } from 'swiper/element';
 @Component({
   selector: 'app-testimonial',
   imports: [],
   templateUrl: './testimonial.component.html',
-  styleUrl: './testimonial.component.css'
+  styleUrl: './testimonial.component.css',
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ]
 })
 export class TestimonialComponent {
+  @ViewChild('testimonialSwiper') swiper!: ElementRef<SwiperContainer>;
 
+  testimonials = [
+    {
+      image: 'images/potonabil.jpeg',
+      name: `Nabil Fadilah`,
+      role: 'Mahasiswa',
+      graduationDate: 2024,
+      testimonial: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+    },
+    {
+      image: 'images/helo.jpeg',
+      name: `Alif Razan Saputra`,
+      role: 'Guru',
+      graduationDate: 2024,
+      testimonial: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.`
+    },
+  ]
 }
