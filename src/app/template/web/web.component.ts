5667152d7d56a006586133dab989fcfd27a1dee8
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-27 11:13:14 +0700

    Mengimport component banner untuk di tampilkan

--- src/app/template/web/web.component.ts
+++ src/app/template/web/web.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
+import { BannerComponent } from '../../components/banner/banner.component';
 
 @Component({
   selector: 'app-web',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, BannerComponent],
   templateUrl: './web.component.html',
   styleUrl: './web.component.css'
 })
