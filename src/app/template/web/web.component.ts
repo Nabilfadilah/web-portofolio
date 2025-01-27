cd6eb55c70e66333c00f067e2bbcc4ccbb591924
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-27 11:05:15 +0700

    Mengimport component header untuk di tampilkan

---
+++ src/app/template/web/web.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+
+@Component({
+  selector: 'app-web',
+  imports: [HeaderComponent],
+  templateUrl: './web.component.html',
+  styleUrl: './web.component.css'
+})
+export class WebComponent {
+
+}
