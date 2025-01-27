88d8f6e9ddc9871a8dc902608e91220599d43c9a
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-27 12:53:12 +0700

    Merubah import menjadi path alias

--- src/app/template/web/web.component.ts
+++ src/app/template/web/web.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from '../../components/header/header.component';
-import { BannerComponent } from '../../components/banner/banner.component';
-import { HeaderMenuComponent } from "../../components/header-menu/header-menu.component";
+import { HeaderComponent } from '@components/header/header.component';
+import { BannerComponent } from '@components/banner/banner.component';
+import { HeaderMenuComponent } from "@components/header-menu/header-menu.component";
 
 @Component({
   selector: 'app-web',
