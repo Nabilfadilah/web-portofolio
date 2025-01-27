51bc570e195ffd142476f281bece2bd1d58335de
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-27 21:33:06 +0700

    Import elementRef, HostListener, dan OnInit serta menjalankan fungsinya

--- src/app/template/web/web.component.ts
+++ src/app/template/web/web.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit } from '@angular/core';
 import { HeaderComponent } from '@components/header/header.component';
 import { BannerComponent } from '@components/banner/banner.component';
 import { HeaderMenuComponent } from "@components/header-menu/header-menu.component";
@@ -19,6 +19,30 @@ import { AchievementComponent } from "../../components/achievement/achievement.c
   templateUrl: './web.component.html',
   styleUrl: './web.component.css'
 })
-export class WebComponent {
+export class WebComponent implements OnInit {
+  bannerEl: any;
+  headerEl: any;
 
+  // panggil target dengan menggunakan elementRef
+  constructor(
+    private el: ElementRef
+  ) { }
+
+  // biar ada booleannya
+  bannerIsScroll: boolean = false;
+  // aboutSecIsScroll: boolean = false;
+
+  ngOnInit(): void {
+    this.headerEl = this.el.nativeElement.querySelector('#headerId').offsetTop;
+    this.bannerEl = this.el.nativeElement.querySelector('#bannerId').offsetTop;
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.bannerIsScroll = window.scrollY > this.headerEl;
+    console.log('this.bannerIsScroll', this.bannerIsScroll);
+    console.log('this.headerEl', this.headerEl);
+    console.log('window.scrolly', window.scrollY);
+    // this.aboutSecIsScroll = window.scrollX > this.bannerEl;
+  }
 }
