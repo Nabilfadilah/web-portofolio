652365739aca21312a8c452d2b4ef9ee445ff558
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-27 11:16:11 +0700

    Mengimport component header menu untuk di tampilkan

--- src/app/template/web/web.component.ts
+++ src/app/template/web/web.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { BannerComponent } from '../../components/banner/banner.component';
+import { HeaderMenuComponent } from "../../components/header-menu/header-menu.component";
 
 @Component({
   selector: 'app-web',
-  imports: [HeaderComponent, BannerComponent],
+  imports: [HeaderComponent, BannerComponent, HeaderMenuComponent],
   templateUrl: './web.component.html',
   styleUrl: './web.component.css'
 })
