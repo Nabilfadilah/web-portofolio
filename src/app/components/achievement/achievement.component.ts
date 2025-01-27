ea18da9b77e088324c45b1db0d6073622c0f57e1
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-27 20:34:48 +0700

    Inport NgFor dan membuat array achievement

--- src/app/components/achievement/achievement.component.ts
+++ src/app/components/achievement/achievement.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-achievement',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './achievement.component.html',
   styleUrl: './achievement.component.css'
 })
 export class AchievementComponent {
-
+  // Array data penghargaan
+  achievements = [
+    {
+      image: 'images/achievement1.png',
+      title: 'Penghargaan Pelayanan Terbaik 2023'
+    },
+    {
+      image: 'images/achievement2.jpg',
+      title: 'Penghargaan Rumah Sakit Ramah Lingkungan'
+    },
+    {
+      image: 'images/achievement1.png',
+      title: 'Penghargaan Inovasi Teknologi Kesehatan'
+    },
+    {
+      image: 'images/achievement2.jpg',
+      title: 'Piagam Akreditasi Rumah Sakit 2022'
+    }
+  ];
 }
