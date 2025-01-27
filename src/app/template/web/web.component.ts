4dbda4bba489b59322462cc9b88aa2c00ad2161b
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-27 16:27:07 +0700

    Mengimport component news untuk di tampilkan

--- src/app/template/web/web.component.ts
+++ src/app/template/web/web.component.ts
@@ -3,10 +3,11 @@ import { HeaderComponent } from '@components/header/header.component';
 import { BannerComponent } from '@components/banner/banner.component';
 import { HeaderMenuComponent } from "@components/header-menu/header-menu.component";
 import { AboutUsComponent } from "../../components/about-us/about-us.component";
+import { NewsComponent } from "../../components/news/news.component";
 
 @Component({
   selector: 'app-web',
-  imports: [HeaderComponent, BannerComponent, HeaderMenuComponent, AboutUsComponent],
+  imports: [HeaderComponent, BannerComponent, HeaderMenuComponent, AboutUsComponent, NewsComponent],
   templateUrl: './web.component.html',
   styleUrl: './web.component.css'
 })
