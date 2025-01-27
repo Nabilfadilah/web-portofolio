a3b1f84de0ce1e6c4082619f5961ad03bf75922c
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-27 12:58:59 +0700

    Import NgFor dan membuat listImages untuk banner

--- src/app/components/banner/banner.component.ts
+++ src/app/components/banner/banner.component.ts
@@ -1,13 +1,18 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-banner',
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, NgFor],
   templateUrl: './banner.component.html',
   styleUrl: './banner.component.css',
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class BannerComponent {
-
+  listImages = [
+    'images/bg1.jpg',
+    'images/bg2.jpg',
+    'images/bg3.jpg'
+  ]
 }
